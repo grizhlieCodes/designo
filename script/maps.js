1cd63ab42ea6979571ef00b824aebfab74f9e73b
grizhlieCodes <rafal.potasz@gmail.com>
2021-02-19 00:27:00 +0000

    Completed contact page with custom form.

--- script/maps.js
+++ script/maps.js
@@ -1,4 +1,7 @@
-var mymap = L.map('mapidddd').setView([51.505, -0.09], 13);
+var canadaMap = L.map('canada').setView([43.644225902294174, -79.39458231726809], 13);
+var australiaMap = L.map('australia').setView([-30.329268835386596, 149.78823453395083], 13);
+var unitedKingdomMap = L.map('unitedKingdom').setView([53.71037373098736, -1.3418136741276538], 13);
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -7,4 +10,22 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ3JpemhsaWUiLCJhIjoiY2tsYjVheTVlMGI5ajJ1bzE4YXVjdTQxNSJ9.q1WaC98laBtuY4gbGubdIw'
-}).addTo(mymap);
+}).addTo(canadaMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3JpemhsaWUiLCJhIjoiY2tsYjVheTVlMGI5ajJ1bzE4YXVjdTQxNSJ9.q1WaC98laBtuY4gbGubdIw'
+}).addTo(australiaMap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3JpemhsaWUiLCJhIjoiY2tsYjVheTVlMGI5ajJ1bzE4YXVjdTQxNSJ9.q1WaC98laBtuY4gbGubdIw'
+}).addTo(unitedKingdomMap);
--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.menu-button')
 const spans = Array.from(burger.querySelectorAll('span'))
-const disNone = Array.from(document.querySelectorAll('.dis-none'))
+const inactives = Array.from(document.querySelectorAll('.inactive'))
+
 
 burger.addEventListener('click', () => {
 
@@ -8,12 +9,33 @@ burger.addEventListener('click', () => {
         span.classList.toggle('clicked')
     })
 
-    disNone.forEach(el => {
-        if(el.classList.contains('dark-backdrop')){
-            el.classList.toggle('dis-opa')
+    inactives.forEach(inactive => {
+        if (inactive.classList.contains('navigation-links')) {
+            inactive.classList.toggle('inactive')
+            inactive.classList.toggle('active')
         } else {
-            el.classList.toggle('dis-full')
+            inactive.classList.toggle('inactive')
         }
     })
 
+})
+
+
+
+
+//error message on email 
+
+const error = document.querySelector('.error')
+const form = document.querySelector('form')
+const email = document.querySelector('#email')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(email.value.length == 0){
+        error.classList.add('true')
+        error.classList.remove('false')
+    } else {
+        error.classList.remove('true')
+        error.classList.add('false')
+    }
 })
